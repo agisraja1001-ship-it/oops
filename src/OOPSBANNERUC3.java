b2fe62d7913eae8d8bc08c348bd13188cba1da33
Agis <agis@Agis-MacBook-Air-4.local>
2026-03-10 20:00:45 +0530

    uc3

---
+++ src/OOPSBANNERUC3.java
@@ -0,0 +1,46 @@
+public class OOPSBANNERUC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " ********",
+                " ********"));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *      *",
+                " *       "));
+
+        System.out.println(String.join(" ",
+                "*       *",
+                "*       *",
+                " *      *",
+                " *       "));
+
+        System.out.println(String.join(" ",
+                "*       *",
+                "*       *",
+                " ********",
+                "  *****  "));
+
+        System.out.println(String.join(" ",
+                "*       *",
+                "*       *",
+                " *       ",
+                "       * "));
+
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *     * "));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  *****  "));
+    }
+}
